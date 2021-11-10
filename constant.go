a9ca67fa751021c5cfb866801ed2a02b918ce6cd
awatercolorpen <hellis@foxmail.com>
2021-11-10 11:19:10 +0800

    update go mod

--- constant.go
+++ constant.go
@@ -5,9 +5,6 @@ const (
 
 	SuperadminRole   = "superadmin"
 	SuperadminDomain = "superdomain"
-
-	ModelConfPathSuperadmin   = "http://raw.githubusercontent.com/awatercolorpen/caskin/main/configs/casbin_model.conf"
-	ModelConfPathNoSuperadmin = "http://raw.githubusercontent.com/awatercolorpen/caskin/main/configs/casbin_model.no_superadmin.conf"
 )
 
 const (
