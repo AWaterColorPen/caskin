5bf09c0951e3ff91e017aeaf4a37db543a1105fe
awatercolorpen <hellis@foxmail.com>
2022-04-21 14:07:27 +0800

    delete some file

--- casbin.go
+++ casbin.go
@@ -1,8 +1,18 @@
 package caskin
 
 import (
+	_ "embed"
+
 	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/model"
 )
+
+//go:embed configs/casbin_model.conf
+var CasbinModelText string
+
+func CasbinModel() (model.Model, error) {
+	return model.NewModelFromString(CasbinModelText)
+}
 
 type IEnforcer interface {
 	Enforce(User, Object, Domain, Action) (bool, error)
--- schema_buildin.go
+++ schema_buildin.go
@@ -38,3 +38,77 @@ func (o *NamedObject) SetDomainID(uint64) {
 func (o *NamedObject) GetObjectType() string {
 	return ""
 }
+
+type SampleSuperadminRole struct {
+	ID   uint64 `json:"id"`
+	Name string `json:"name"`
+}
+
+func (s *SampleSuperadminRole) GetID() uint64 {
+	return s.ID
+}
+
+func (s *SampleSuperadminRole) SetID(uint64) {
+}
+
+func (s *SampleSuperadminRole) Encode() string {
+	return SuperadminRole
+}
+
+func (s *SampleSuperadminRole) Decode(code string) error {
+	if code != SuperadminRole {
+		return ErrIsNotSuperadmin
+	}
+	return nil
+}
+
+func (s *SampleSuperadminRole) GetObjectID() uint64 {
+	return 0
+}
+
+func (s *SampleSuperadminRole) SetObjectID(uint64) {
+}
+
+func (s *SampleSuperadminRole) GetDomainID() uint64 {
+	return 0
+}
+
+func (s *SampleSuperadminRole) SetDomainID(uint64) {
+}
+
+type SampleSuperadminDomain struct {
+	ID   uint64 `json:"id"`
+	Name string `json:"name"`
+}
+
+func (s *SampleSuperadminDomain) GetID() uint64 {
+	return s.ID
+}
+
+func (s *SampleSuperadminDomain) SetID(uint64) {
+}
+
+func (s *SampleSuperadminDomain) Encode() string {
+	return SuperadminDomain
+}
+
+func (s *SampleSuperadminDomain) Decode(code string) error {
+	if code != SuperadminDomain {
+		return ErrIsNotSuperadmin
+	}
+	return nil
+}
+
+func GetSuperadminRole() Role {
+	return &SampleSuperadminRole{
+		ID:   0,
+		Name: DefaultSuperadminRoleName,
+	}
+}
+
+func GetSuperadminDomain() Domain {
+	return &SampleSuperadminDomain{
+		ID:   0,
+		Name: DefaultSuperadminDomainName,
+	}
+}
